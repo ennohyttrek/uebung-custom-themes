cf7c4d907379004a0898855fba70926ebaab9d64
Enno Hyttrek <ehy.global@gmail.com>
2021-05-25 23:14:37 +0200

    Update 5/2021

--- schritt01/index.php
+++ schritt01/index.php
@@ -3,7 +3,7 @@
     
     <head>
         <title>Statisches HTML</title>
-    <?php wp_head() ?>
+
     </head>
 
     <body>
@@ -14,7 +14,7 @@
         Dies ist ein Typoblindtext. An ihm kann man sehen, ob alle Buchstaben da sind und wie sie aussehen. Manchmal benutzt man Worte wie Hamburgefonts, Rafgenduks oder Handgloves, um Schriften zu testen. Manchmal Sätze, die alle Buchstaben des Alphabets enthalten - man nennt diese Sätze »Pangrams«. Sehr bekannt ist dieser: The quick brown fox jumps over the lazy old dog.
         </p>
 
-    <?php wp_footer() ?>
+  
     </body>
     
 </html>
